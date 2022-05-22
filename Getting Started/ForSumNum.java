6a3688347a776591973b29b5736c091f2a9301d4
Saurabh <saurabhchavhan07@gmail.com>
2022-05-22 15:22:08 +0530

    For loop progrm to print sum of n number.

---
+++ Getting Started/ForSumNum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ForSumNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        int num = sc.nextInt();
+        int sum = 0;
+
+        for(int i = 1; i<=num; i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+
+        }
+    
+}
